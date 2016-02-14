516c5f87381ae4b8cc3d2e35267029e09a0a4f7e
Trollpower <github@trollpower.de>
2016-02-14 18:27:59 +0100

    Removed not needed modules and functions to start with a simple setup

--- app/src/main/java/com/morristaedt/mirror/SetUpActivity.java
+++ app/src/main/java/com/morristaedt/mirror/SetUpActivity.java
@@ -22,31 +22,11 @@ import com.morristaedt.mirror.configuration.ConfigurationSettings;
 
 public class SetUpActivity extends Activity {
 
-    private static final long HOUR_MILLIS = 60 * 60 * 1000;
-    private static final int METERS_MIN = 500;
-
     @NonNull
     private ConfigurationSettings mConfigSettings;
-
-    private LocationManager mLocationManager;
-
-    @Nullable
-    private LocationListener mLocationListener;
-
-    @Nullable
-    private Location mLocation;
-
-    private RadioGroup mTemperatureChoice;
-    private CheckBox mBikingCheckbox;
-    private CheckBox mMoodDetectionCheckbox;
     private CheckBox mShowNextCaledarEventCheckbox;
-    private CheckBox mShowNewsHeadlineCheckbox;
     private CheckBox mXKCDCheckbox;
     private CheckBox mXKCDInvertCheckbox;
-    private View mLocationView;
-    private EditText mLatitude;
-    private EditText mLongitude;
-    private EditText mStockTickerSymbol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,20 +35,8 @@ public class SetUpActivity extends Activity {
 
         mConfigSettings = new ConfigurationSettings(this);
 
-        mTemperatureChoice = (RadioGroup) findViewById(R.id.temperature_group);
-        mTemperatureChoice.check(mConfigSettings.getIsCelsius() ? R.id.celsius : R.id.farenheit);
-
-        mBikingCheckbox = (CheckBox) findViewById(R.id.biking_checkbox);
-        mBikingCheckbox.setChecked(mConfigSettings.showBikingHint());
-
-        mMoodDetectionCheckbox = (CheckBox) findViewById(R.id.mood_detection_checkbox);
-        mMoodDetectionCheckbox.setChecked(mConfigSettings.showMoodDetection());
-
         mShowNextCaledarEventCheckbox = (CheckBox) findViewById(R.id.calendar_checkbox);
         mShowNextCaledarEventCheckbox.setChecked(mConfigSettings.showNextCalendarEvent());
-
-        mShowNewsHeadlineCheckbox = (CheckBox) findViewById(R.id.headline_checkbox);
-        mShowNewsHeadlineCheckbox.setChecked(mConfigSettings.showNewsHeadline());
 
         mXKCDCheckbox = (CheckBox) findViewById(R.id.xkcd_checkbox);
         mXKCDCheckbox.setChecked(mConfigSettings.showXKCD());
@@ -76,25 +44,10 @@ public class SetUpActivity extends Activity {
         mXKCDInvertCheckbox = (CheckBox) findViewById(R.id.xkcd_invert_checkbox);
         mXKCDInvertCheckbox.setChecked(mConfigSettings.invertXKCD());
 
-        mLatitude = (EditText) findViewById(R.id.latitude);
-        mLongitude = (EditText) findViewById(R.id.longitude);
-
-        mLatitude.setText(String.valueOf(mConfigSettings.getLatitude()));
-        mLongitude.setText(String.valueOf(mConfigSettings.getLongitude()));
-
-        mLocationView = findViewById(R.id.location_view);
-        setUpLocationMonitoring();
-
-        mStockTickerSymbol = (EditText) findViewById(R.id.stock_name);
-        mStockTickerSymbol.setText(mConfigSettings.getStockTickerSymbol());
-
         findViewById(R.id.launch_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 saveFields();
-
-                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-                imm.hideSoftInputFromWindow(mStockTickerSymbol.getWindowToken(), 0);
 
                 Intent intent = new Intent(SetUpActivity.this, MirrorActivity.class);
                 startActivity(intent);
@@ -102,77 +55,8 @@ public class SetUpActivity extends Activity {
         });
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        if (mLocationManager != null && mLocationListener != null) {
-            mLocationManager.removeUpdates(mLocationListener);
-        }
-    }
-
-    private void setUpLocationMonitoring() {
-        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-        String provider = mLocationManager.getBestProvider(criteria, true);
-
-        try {
-            mLocation = mLocationManager.getLastKnownLocation(provider);
-
-            if (mLocation == null) {
-                mLocationView.setVisibility(View.VISIBLE);
-                mLocationListener = new LocationListener() {
-                    @Override
-                    public void onLocationChanged(Location location) {
-                        if (location != null) {
-                            Toast.makeText(SetUpActivity.this, R.string.found_location, Toast.LENGTH_SHORT).show();
-                            mLocation = location;
-                            mConfigSettings.setLatLon(String.valueOf(mLocation.getLatitude()), String.valueOf(mLocation.getLongitude()));
-                            mLocationManager.removeUpdates(this);
-                            if (mLocationView != null) {
-                                mLocationView.setVisibility(View.GONE);
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-                    }
-
-                    @Override
-                    public void onProviderEnabled(String provider) {
-
-                    }
-
-                    @Override
-                    public void onProviderDisabled(String provider) {
-
-                    }
-                };
-                mLocationManager.requestLocationUpdates(provider, HOUR_MILLIS, METERS_MIN, mLocationListener);
-            } else {
-                mLocationView.setVisibility(View.GONE);
-            }
-        } catch (IllegalArgumentException e) {
-            Log.e("SetUpActivity", "Location manager could not use provider", e);
-        }
-    }
-
     private void saveFields() {
-        mConfigSettings.setIsCelsius(mTemperatureChoice.getCheckedRadioButtonId() == R.id.celsius);
-        mConfigSettings.setShowBikingHint(mBikingCheckbox.isChecked());
-        mConfigSettings.setShowMoodDetection(mMoodDetectionCheckbox.isChecked());
         mConfigSettings.setShowNextCalendarEvent(mShowNextCaledarEventCheckbox.isChecked());
-        mConfigSettings.setShowNewsHeadline(mShowNewsHeadlineCheckbox.isChecked());
         mConfigSettings.setXKCDPreference(mXKCDCheckbox.isChecked(), mXKCDInvertCheckbox.isChecked());
-
-        if (mLocation == null) {
-            mConfigSettings.setLatLon(mLatitude.getText().toString(), mLongitude.getText().toString());
-        } else {
-            mConfigSettings.setLatLon(String.valueOf(mLocation.getLatitude()), String.valueOf(mLocation.getLongitude()));
-        }
-
-        mConfigSettings.setStockTickerSymbol(mStockTickerSymbol.getText().toString());
     }
 }
